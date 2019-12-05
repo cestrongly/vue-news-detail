45bb379546edc2988ae3067e263c7c94e768c41a
DESKTOP-G10M43M\Cest <cestrmail@163.com>
2019-12-05 10:27:58 +0800

    update: vue实战——路由的导航守卫与从后台获取数据

--- src/router/index.js
+++ src/router/index.js
@@ -16,12 +16,18 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () =>
+      import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
     path: '/newAbout',
     name: 'newAbout',
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () =>
+      import(/* webpackChunkName: "about" */ '../views/newAbout.vue'),
+    beforeEnter: (to, from, next) => {
+      console.log('newAbout独享的前置守卫')
+      next()
+    }
   }
 ]
 
@@ -36,7 +42,6 @@ router.beforeEach((to, from, next) => {
   console.log(to)
   console.log(from)
   console.log(next)
-  debugger
   next()
 })
 
@@ -44,7 +49,6 @@ router.afterEach((to, from) => {
   console.log('全局后置钩子')
   console.log(to)
   console.log(from)
-  debugger
 })
 
 export default router
